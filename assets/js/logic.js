0d1f4ef0d9e56b72795b73014caf7ee77a199093
Jacob <Jakelausier7878@gmail.com>
2023-07-14 15:30:06 -0400

    added example card of wher yelp will display

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,7 +2,7 @@ var games = document.getElementById('game-search-form-game')
 var loacation = document.getElementById('game-search-form-location')
 var buttonEl = document.getElementById('game-search-form-submit')
 var formEl = document.getElementById('main-search-game')
-
+var yelpEl = document.getElementById('yelp-container')
 
 
 // button gets values from both inputs
@@ -20,7 +20,7 @@ $(buttonEl).on('click', function(event){
 // };
 
 
-movement(){
+function movement(){
 
     formEl.classList.remove('items-center')
     formEl.classList.add('p-10', 'text-center', 'w-80')
@@ -29,3 +29,12 @@ movement(){
     
 
 };
+// function with for loop fo reach yelp element returned
+function displayYelp(){
+yelpList = document.createElement('li')
+yelpList.id = "yelp-cards"
+ 
+let listEL = document.getElementById('yelp-cards')
+listEL.setAttribute('class', '')
+
+}
