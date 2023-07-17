b1b6a41bb74346398337d92fe66853042d4684db
alfaro-matttthew <137105768+alfaro-matttthew@users.noreply.github.com>
2023-07-17 08:11:34 -0400

    rewrote an html file that can be used for one page

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,31 +1,32 @@
-var games = document.getElementById('game-search')
-var loacation = document.getElementById('location-search')
-var buttonEl = document.getElementById('search-btn')
-var formEl = document.getElementById('main-search-game')
-var yelpEl = document.getElementById('yelp-container')
+$(document).ready (function() {
+
+    var games = $('#game-search');
+    var loacation = $('#location-search');
+    var buttonEl = $('#search-btn')
+    // var formEl = $('#main-search-game')
+    // var yelpEl = $('#yelp-container')
 
 
-// button gets values from both inputs
-$(buttonEl).on('click', function(){
-    // let gameInput = games.value
-    // let locationInput  = location.value
-    //      (gameInput)
-    //      (locationInput);
-   redirect()
-})
 
-// dateResults(gameInput, locationInput){
-// //fetch
-function redirect(event) {
+    function saveSearchValues() {
 
-window.location.href = "/results.html";
+        // takes values from the form
+        var gameName = games.val();
+        var cityName = loacation.val();
+        console.log(gameName);
+        console.log(cityName);
 
-}
+        // places form values into an array
+        let searchedItems = [gameName, cityName];
+        console.log(searchedItems);
 
-// };     
+        // uploads the 
+        localStorage.setItem('Recent Search', JSON.stringify(searchedItems));
 
-// This is a test
-   
+        //
 
+    }
 
-// function with for loop fo reach yelp element returned
+    buttonEl.on('click', saveSearchValues);
+
+});
